34ba90b7d494e65ad81a875e8b3165ab60c021f7
rolando22 <rolando2207@gmail.com>
2023-07-27 22:56:29 -0300

    [ADD] rollback del estado al eliminar usuario

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,44 @@
-import { configureStore } from "@reduxjs/toolkit";
-import usersReducer from './users/slice';
+import { configureStore, type Middleware } from "@reduxjs/toolkit";
+import usersReducer, { rollbackUser } from './users/slice';
+import { toast } from "sonner";
 
-const persistanceLocalStorageMiddleware = (store) => (next) => (action) => {
+const persistanceLocalStorageMiddleware: Middleware = (store) => (next) => (action) => {
     next(action);
     localStorage.setItem('users_redux', JSON.stringify(store.getState()));
+};
+
+const syncWithDatabaseMiddleware: Middleware = (store) => (next) => (action) => {
+    const { type, payload } = action;
+    const prevState = store.getState() as RootState;
+    next(action);
+    if (type === 'users/deleteUserById') {
+        const userIdToRemove = payload;
+        const userToRemove = prevState.users.find(user => user.id === userIdToRemove);
+        fetch(`https://jsonplaceholder.typicode.com/users/${userIdToRemove}`, {
+			method: 'DELETE'
+		})
+            .then(res => {
+                if (res.ok)
+                    toast.success(`Usuario ${userIdToRemove} eliminado correctamente`);
+                else
+                    throw new Error('Error al eliminar el usuario');
+            })
+            .catch(err => {
+                toast.error(`Error al eliminar el usuario ${userIdToRemove}`);
+                if (userToRemove) store.dispatch(rollbackUser(userToRemove));
+                console.log(err);
+            });
+    }
 };
 
 export const store = configureStore({
     reducer: {
         users: usersReducer,
     },
-    middleware: [persistanceLocalStorageMiddleware],
+    middleware: [
+        persistanceLocalStorageMiddleware, 
+        syncWithDatabaseMiddleware, 
+    ],
 });
 
 export type RootState = ReturnType<typeof store.getState>;
--- src/store/users/slice.ts
+++ src/store/users/slice.ts
@@ -34,14 +34,18 @@ export const usersSlice = createSlice({
     reducers: {
         addNewUser: (state, action: PayloadAction<User>) => {
             const id = crypto.randomUUID();
-            return [...state, { id, ...action.payload }];
+            state.push({ id, ...action.payload });
         },
         deleteUserById: (state, action: PayloadAction<UserId>) => {
             const id = action.payload;
             return state.filter(user => user.id !== id);
         },
+        rollbackUser: (state, action: PayloadAction<UserWithId>) => {
+            const isUserAlreadyDefined = state.some(user => user.id === action.payload.id);
+            if (!isUserAlreadyDefined) state.push({ ...action.payload });
+        },
     },
 });
 
 export default usersSlice.reducer;
-export const { addNewUser, deleteUserById } = usersSlice.actions;
+export const { addNewUser, deleteUserById, rollbackUser } = usersSlice.actions;
